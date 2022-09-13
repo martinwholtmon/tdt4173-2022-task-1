9d11d5ace4265811ad2281993e85ee6f3af13f6f
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2022-09-13 16:43:07 +0200

    implement some code to find the best attribute for the root node

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,56 +1,104 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
+
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
 
 
 class DecisionTree:
-    
     def __init__(self):
-        # NOTE: Feel free add any hyperparameters 
+        # NOTE: Feel free add any hyperparameters
         # (with defaults) as you see fit
         pass
-    
+
     def fit(self, X, y):
         """
         Generates a decision tree for classification
-        
+
         Args:
             X (pd.DataFrame): a matrix with discrete value where
                 each row is a sample and the columns correspond
                 to the features.
             y (pd.Series): a vector of discrete ground-truth labels
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
+        # Prepare arrays
+        attributes = np.asarray(X.columns)
+        print(attributes)
+        X = np.asarray(X)
+        y = np.asarray(y)
+        n_examples, n_attributes = X.shape
+
+        print(X)
+        # print(y)
+        positive = 0
+        negative = 0
+        for i in range(n_examples):
+            if y[i] == "Yes":
+                positive += 1
+            else:
+                negative += 1
+        S = np.array([positive, negative])
+
+        # id3
+        root = ["test"]
+        pos = 0
+        curr_entropy = 0
+        curr_pos = 0
+        for attribute in range(n_attributes):
+            # get occurances
+            arr = {}
+            total = 0
+            for example in range(n_examples):
+                key = X[example][attribute]
+                if key not in arr:
+                    arr[key] = [0, 0]
+                if y[example] == "Yes":
+                    arr[key] = [arr[key][0] + 1, arr[key][1]]
+                else:
+                    arr[key] = [arr[key][0], arr[key][1] + 1]
+                total += 1
+
+            # pick attribute
+            ent = entropy(S)
+            for _, v in arr.items():
+                v = np.asarray(v)
+                ent = ent - sum(v) / total * entropy(v)
+            print(f"Entrophy for attribute {attributes[attribute]} = {ent}")
+
+            if ent > curr_entropy:
+                root[pos] = attributes[attribute]
+                curr_entropy = ent
+                curr_pos = attribute
+        np.delete(attributes, curr_pos)
+        pos += 1
+
     def predict(self, X):
         """
         Generates predictions
-        
+
         Note: should be called after .fit()
-        
+
         Args:
             X (pd.DataFrame): an mxn discrete matrix where
                 each row is a sample and the columns correspond
                 to the features.
-            
+
         Returns:
             A length m vector with predictions
         """
-        # TODO: Implement 
+        # TODO: Implement
         raise NotImplementedError()
-    
+
     def get_rules(self):
         """
         Returns the decision tree as a list of rules
-        
+
         Each rule is given as an implication "x => y" where
         the antecedent is given by a conjuction of attribute
         values and the consequent is the predicted label
-        
+
             attr1=val1 ^ attr2=val2 ^ ... => label
-        
+
         Example output:
         >>> model.get_rules()
         [
@@ -63,16 +111,17 @@ class DecisionTree:
         raise NotImplementedError()
 
 
-# --- Some utility functions 
-    
+# --- Some utility functions
+
+
 def accuracy(y_true, y_pred):
     """
     Computes discrete classification accuracy
-    
+
     Args:
         y_true (array<m>): a length m vector of ground truth labels
         y_pred (array<m>): a length m vector of predicted labels
-        
+
     Returns:
         The average number of correct predictions
     """
@@ -83,7 +132,7 @@ def accuracy(y_true, y_pred):
 def entropy(counts):
     """
     Computes the entropy of a partitioning
-    
+
     Args:
         counts (array<k>): a lenth k int array >= 0. For instance,
             an array [3, 4, 1] implies that you have a total of 8
@@ -91,16 +140,13 @@ def entropy(counts):
             and 1 one in the last. This will result in entropy > 0.
             In contrast, a perfect partitioning like [8, 0, 0] will
             result in a (minimal) entropy of 0.0
-            
+
     Returns:
         A positive float scalar corresponding to the (log2) entropy
         of the partitioning.
-    
+
     """
     assert (counts >= 0).all()
     probs = counts / counts.sum()
     probs = probs[probs > 0]  # Avoid log(0)
-    return - np.sum(probs * np.log2(probs))
-
-
-
+    return -np.sum(probs * np.log2(probs))
